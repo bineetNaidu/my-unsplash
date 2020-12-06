6c9e1aaa5ed563f57950008a3554e47d7ad95135
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-06 22:11:51 +0530

    Add signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,12 +1,15 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
+import User from "../model/User";
 
 const router = Router();
 
 router.post(
   "/api/users/signup",
   [
+    body("username").isString().withMessage("Username must be valid"),
+    body("hobby").isString().withMessage("Hobby must be valid"),
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
@@ -14,12 +17,23 @@ router.post(
       .withMessage("Password must be between 6 and 20 Characters"),
   ],
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { username, email, password, hobby } = req.body;
+
+      const user = await new User({ username, email, password, hobby }).save();
+      const token = jwt.sign({ user }, "qwerty", {
+        expiresIn: 3 * 24 * 60 * 60, // ? 3 days,
+      });
+
+      res.cookie("jwt", token);
+      res.json({ status: "OK", user });
+    } catch (e) {
+      res.status(400).json({ errors: [{ msg: e.message }] });
     }
-    const { username, email, password, hobby } = req.body;
-    res.json({ status: "OK", ...req.body });
   }
 );
 
