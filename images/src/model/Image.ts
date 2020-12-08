7f18431b738ce791cf593be904cbbf651e996cde
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-08 23:45:35 +0530

    add Image model w/ types

---
+++ images/src/model/Image.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+interface ImageDoc extends mongoose.Document {
+  url: string;
+  name: string;
+  description: string;
+  uploadedBy: string;
+}
+
+const ImageSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  uploadedBy: {
+    type: mongoose.Types.ObjectId,
+    require: true,
+  },
+});
+
+export default mongoose.model<ImageDoc>("Image", ImageSchema);
