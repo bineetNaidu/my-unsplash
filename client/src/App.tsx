5477eb42230aacf6b85fbb166cf3cc1d310ed4e5
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-08 22:25:05 +0530

    - add signup handlers -- auth checkins for user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Axios from "axios";
 import Header from "./Header";
 import Home from "./Home";
 import Signup from "./Signup";
+import { User } from "./types";
 
 const App: FC = () => {
+  const [user, setUser] = useState<User | {}>({});
+  useEffect(() => {
+    (async () => {
+      const authUser = await Axios.get("/api/users/currentuser");
+      setUser(authUser.data.currentUser.user);
+    })();
+  }, [setUser]);
   return (
     <>
       <Header />
--- client/src/Signup.tsx
+++ client/src/Signup.tsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
 import "./Signup.css";
 
 const Signup: React.FC = () => {
+  const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [hobby, setHobby] = useState("");
+
+  const handleSignup = async () => {
+    const { data } = await Axios.post("/api/users/signup", {
+      email,
+      username,
+      password,
+      hobby,
+    });
+    if (data.errors) {
+      return alert(data.errors);
+    }
+    history.push("/");
+  };
+
   return (
-    <form className="auth__form">
+    <form
+      className="auth__form"
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await handleSignup();
+      }}
+    >
       <h1>Sign up</h1>
       <div className="form__box">
         <label>Username</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div className="form__box">
         <label>Email</label>
-        <input type="email" />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form__box">
         <label>Password</label>
-        <input type="password" />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+        />
       </div>
       <div className="form__box">
         <label>Hobby</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={hobby}
+          onChange={(e) => setHobby(e.target.value)}
+        />
       </div>
       <button type="submit">Signup!</button>
     </form>
