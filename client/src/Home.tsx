e92f06ccf848e762f82939b2f7828fab36607da5
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-11 22:56:50 +0530

    add delete method

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import Image from "./Image";
-import { User } from "./types";
+import { Image as ImageInterface, User } from "./types";
 import Modal from "./Modal";
 
 interface Props {
@@ -20,13 +20,22 @@ const Home: React.FC<Props> = ({ user, setVisible, visible }) => {
     })();
   }, []);
 
+  const handleDelete = async (_id: string) => {
+    const { data } = await Axios.delete(`/api/images/${_id}`);
+    if (data.status === "OK")
+      setImages(images.filter((i: ImageInterface) => i._id !== _id));
+  };
+
   return (
     <div>
       {visible && (
         <Modal images={images} setImages={setImages} setVisible={setVisible} />
       )}
       <div className="mansonry" style={{}}>
-        {images && images.map((img) => <Image image={img} user={user} />)}
+        {images &&
+          images.map((img) => (
+            <Image image={img} user={user} handleDelete={handleDelete} />
+          ))}
       </div>
     </div>
   );
--- client/src/Image.tsx
+++ client/src/Image.tsx
@@ -5,13 +5,17 @@ import "./Image.css";
 interface Props {
   user: User;
   image: ImageInterface;
+  handleDelete: (_id: string) => Promise<void>;
 }
 
-const Image: React.FC<Props> = ({ image, user }) => {
+const Image: React.FC<Props> = ({ image, user, handleDelete }) => {
   const { name, url, _id, uploadedBy } = image;
+
   return (
     <div className="image" key={_id}>
-      {user._id === uploadedBy && <button>X</button>}
+      {user && user._id === uploadedBy && (
+        <button onClick={async () => await handleDelete(_id!)}>X</button>
+      )}
       <img src={url} alt={name} />
       <h4 className="image__name">{name}</h4>
     </div>
