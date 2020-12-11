96964ae64007c0a777073fe2d25199dda77d45f6
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-11 14:56:19 +0530

    add name while hover and add delete button

--- client/src/Image.tsx
+++ client/src/Image.tsx
@@ -4,14 +4,16 @@ import "./Image.css";
 
 interface Props {
   user: User;
-  image: ImageInterface | any;
+  image: ImageInterface;
 }
 
 const Image: React.FC<Props> = ({ image, user }) => {
-  const { name, url, _id } = image;
+  const { name, url, _id, uploadedBy } = image;
   return (
     <div className="image" key={_id}>
+      {user._id === uploadedBy && <button>X</button>}
       <img src={url} alt={name} />
+      <h4 className="image__name">{name}</h4>
     </div>
   );
 };
